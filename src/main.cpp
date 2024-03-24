bfa2f082057e7c7b91d143f85c4a8f64f25a16b5
Davor Brcković <dbrckovi@gmail.com>
2024-03-24 11:34:34 +0100

    Knight rider pattern

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #define RCLK 5
 #define SRCLK 6  
 
-uint8_t value;
+uint8_t value = 1;
+bool going_up = true;
 
 void Show();
 
@@ -26,13 +27,14 @@ void loop()
 {
   shiftOut(SER, SRCLK, HIGH, value);
   Show();
-  delay(5);
-  value++;
+  if (going_up) value*=2;
+  else value/=2;
+
+  if (value == 128 || value == 1) going_up = !going_up;
 }
 
 void Show()
 {
   digitalWrite(RCLK, LOW);
-  delay(10);
   digitalWrite(RCLK, HIGH); 
 }
