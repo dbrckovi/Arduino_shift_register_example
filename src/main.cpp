82f61756f44c2636bcab0b4804f187d612d2f630
Davor Brcković <dbrckovi@gmail.com>
2024-03-24 11:39:49 +0100

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-#define SRCLR 2       //clears memory when going LOW -> HIGH
-#define SER 4         //value is copied to first output pin when SRCLK goes LOW -> HIGH
-#define RCLK 5        //data is copied to output pins when RCLK goes LOW -> HIGH
-#define SRCLK 6       //data from SER pin is copied to first output when SRCLK goes LOW -> HIGH
+#define SRCLR 2       //CLEAR        - clears memory when going LOW -> HIGH
+#define SER 4         //DATA         - value is copied to first output pin when SRCLK goes LOW -> HIGH
+#define RCLK 5        //OUTPUT CLOCK - data is copied to output pins when RCLK goes LOW -> HIGH
+#define SRCLK 6       //INPUT CLOCK  - data from SER pin is copied to first output when SRCLK goes LOW -> HIGH
 
 uint8_t value = 1;
 bool going_up = true;
