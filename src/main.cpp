318913112b6ab7fa7c81e38fb3ede0ac77a54b9f
Davor Brcković <dbrckovi@gmail.com>
2024-04-10 19:53:52 +0200

    Single segment display test

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,100 @@ For this to work, connect shift register (SNx4HC595) like this
  - output pins each to LED -> 1k resistor -> GND
 */
 
-//#define SRCLR 2       //CLEAR        - clears memory when going LOW -> HIGH
-#define SER 4         //DATA         - value is copied to first output pin when SRCLK goes LOW -> HIGH
-#define RCLK 5        //OUTPUT CLOCK - data is copied to output pins when RCLK goes LOW -> HIGH
-#define SRCLK 6       //INPUT CLOCK  - data from SER pin is copied to first output when SRCLK goes LOW -> HIGH
+// #define SRCLR 2       //CLEAR        - clears memory when going LOW -> HIGH
+#define SER 4   // DATA         - value is copied to first output pin when SRCLK goes LOW -> HIGH
+#define RCLK 5  // OUTPUT CLOCK - data is copied to output pins when RCLK goes LOW -> HIGH
+#define SRCLK 6 // INPUT CLOCK  - data from SER pin is copied to first output when SRCLK goes LOW -> HIGH
 
-uint8_t value = 1;
-bool going_up = true;
+void Show(uint8_t value);
+void ShowDigit(byte value);
 
-void Show();
-
-void setup() 
+void setup()
 {
-  //pinMode(SRCLR, OUTPUT);
+  // pinMode(SRCLR, OUTPUT);
   pinMode(SER, OUTPUT);
   pinMode(RCLK, OUTPUT);
   pinMode(SRCLK, OUTPUT);
 
-  //digitalWrite(SRCLR, HIGH);
+  // digitalWrite(SRCLR, HIGH);
   digitalWrite(SER, LOW);
   digitalWrite(RCLK, LOW);
   digitalWrite(SRCLK, LOW);
+
+  Serial.begin(115200);
 }
 
-void loop() 
+void loop()
+{
+  for (byte digit = 0; digit <= 9; digit++)
+  {
+    ShowDigit(digit);
+    delay(300);
+  }
+}
+
+void Show(uint8_t value)
 {
   shiftOut(SER, SRCLK, HIGH, value);
-  Show();
-  if (going_up) value*=2;
-  else value/=2;
-
-  if (value == 128 || value == 1) going_up = !going_up;
+  digitalWrite(RCLK, LOW);
+  digitalWrite(RCLK, HIGH);
+  Serial.println(value);
 }
 
-void Show()
+void ShowDigit(byte digit)
 {
-  digitalWrite(RCLK, LOW);
-  digitalWrite(RCLK, HIGH); 
+  uint8_t outValue = 0;
+
+  /*
+    7
+  6   1
+    2
+  5   3
+    4
+
+  76543210
+  10011110  3
+  01001110  4
+  11011100  5
+  11111100  6
+  10001010  7
+  11111111  8
+  11011111  9
+  */
+
+  switch (digit)
+  {
+  case 0:
+    outValue = 0b11111010;
+    break;
+  case 1:
+    outValue = 0b00001010;
+    break;
+  case 2:
+    outValue = 0b10110110;
+    break;
+  case 3:
+    outValue = 0b10011110;
+    break;
+  case 4:
+    outValue = 0b01001110;
+    break;
+  case 5:
+    outValue = 0b11011100;
+    break;
+  case 6:
+    outValue = 0b11111100;
+    break;
+  case 7:
+    outValue = 0b10001010;
+    break;
+  case 8:
+    outValue = 0b11111111;
+    break;
+  case 9:
+    outValue = 0b11011111;
+    break;
+  }
+
+  Show(outValue);
 }
